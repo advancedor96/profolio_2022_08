fa35aa87d0628b212cb7bbfe37346aae4d15a090
丁丁 <advancedor96@gmail.com>
2022-08-12 00:46:38 +0800

    quick v1

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+<img src="@/assets/images/meetup_202204.jpg" alt="sss">
+<img src="@/assets/images/meetup_202112.jpg" alt="sss">
+<img src="@/assets/images/meetup_202112-2.jpg" alt="sss">
+</div>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <!-- <router-view/> -->
+</template>
+
+<script>
+export default {
+  created () {
+  }
+
+}
+</script>
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+// nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+</style>
